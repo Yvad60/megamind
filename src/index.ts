64f87dafa3aca7a716eb06a4b3ab0ff5be3312fc
ivad <yvesyvad@gmail.com>
2022-06-06 22:53:08 +0200

    ch(setup): prisma and sqlite

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
+import { context } from './context';
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ schema, context });
 const port = process.env.PORT || 3000;
 
 server.listen({ port }).then(({ url }) => {
