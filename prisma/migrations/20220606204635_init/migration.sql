00abf4e3588ce93714178ae34034d64d857dc5c6
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2022-06-09 10:18:36 +0200

    feat(crud): crud operations for users (#4)

--- prisma/migrations/20220606204635_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL,
-    "names" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
